163a22e08b1eed8784ac5a3ec948d61bad010a15
Brian Bower <brian@brianbower.com>
2020-03-19 10:09:38 -0600

    Add navbar component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faGift, // the clock icon
+} from '@fortawesome/free-solid-svg-icons'
+
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1><FontAwesomeIcon icon={faGift} /> GiftyGif Finder</h1>
+      <ul>
+        <li>
+          Home
+        </li>
+        <li>
+          About
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
