3695262f9f48314b5ca9ad51e537d3f5332362e0
Brian Bower <brian@brianbower.com>
2020-03-19 15:35:42 -0600

    Add navbar and search component with axios request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import Navbar from './components/layout/Navbar'
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Search from './components/layout/Search';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  )
+class App extends Component {
+  state = {
+    gifs: []
+  }
+  
+  searchGifs = async (text) => {
+    const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${text}`)
+    
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Search searchGifs={this.searchGifs} />
+      </div>
+    )
+  }
 }
 
 export default App;
