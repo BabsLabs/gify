3adaa86751eea5e9aa48c9dbf74ea94ecd0d0d46
Brian Bower <brian@brianbower.com>
2020-03-20 09:53:10 -0600

    Remove console.log

--- src/components/gifs/GifItem.js
+++ src/components/gifs/GifItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const GifItem = ({ gif: { title, images }}) => {
-  console.log(images)
   return (
       <img src={images.original.url} alt={title} style={{ width: "200px"}} className="gif" />
   )
