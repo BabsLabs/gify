6540201d0c0d8d2a72cdd8ea870104f583af280c
Brian Bower <brian@brianbower.com>
2020-03-19 18:05:52 -0600

    Add gifs component

---
+++ src/components/gifs/Gifs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GifItem from './GifItem'
+
+const Gifs = ({gifs}) => {
+  return (
+    <div className="gifs">
+    {gifs.map(gif => (
+      <GifItem key={gif.id} gif={gif} />
+    ))}
+    </div>
+  )
+}
+
+export default Gifs
