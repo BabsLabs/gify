22770269dd5491d013772a278a2db45f2162c15b
Brian Bower <brian@brianbower.com>
2020-03-20 09:27:52 -0600

    Add loading logic and spinner

--- src/components/gifs/Gifs.js
+++ src/components/gifs/Gifs.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import GifItem from './GifItem';
+import Loading from '../layout/Loading';
 
-const Gifs = ({gifs}) => {
-  return (
-    <div className="gifs">
-    {gifs.map(gif => (
-      <GifItem key={gif.id} gif={gif} />
-    ))}
-    </div>
-  )
+const Gifs = ({gifs, loading}) => {
+  if (loading) {
+    return <Loading />
+  } else {
+    return (
+      <div className="gifs">
+      {gifs.map(gif => (
+        <GifItem key={gif.id} gif={gif} />
+      ))}
+      </div>
+    )
+  }
 }
 
 export default Gifs
