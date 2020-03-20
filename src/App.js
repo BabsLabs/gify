7bd517e9816e9b23b9b404a8b21ac785c6f7b5a6
Brian Bower <brian@brianbower.com>
2020-03-20 09:50:22 -0600

    Add alert if no text is entered

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component, Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Search from './components/layout/Search';
-import Gifs from './components/gifs/Gifs'
+import Gifs from './components/gifs/Gifs';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     gifs: [],
-    loading: false
+    loading: false,
+    alert: null
   }
   
   searchGifs = async (text) => {
-    this.setState({ loading: true })
+    this.setState({ loading: true, alert: null})
     const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${text}`)
     this.setState({ gifs: response.data.data , loading: false })
+  }
+
+  setAlert = (message, type) => {
+    this.setState({ alert: { message, type } })
   }
 
   render() {
@@ -23,8 +29,9 @@ class App extends Component {
         <Navbar />
         <div>
           <Fragment>
-            <Search searchGifs={ this.searchGifs } />
-            <Gifs gifs={ this.state.gifs } loading={ this.state.loading }/>
+            <Search searchGifs={this.searchGifs} setAlert={this.setAlert} />
+            <Alert alert={ this.state.alert } className="alert" />
+            <Gifs gifs={ this.state.gifs } loading={ this.state.loading } />
           </Fragment>
         </div>
       </div>
