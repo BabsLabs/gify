8b8504c61d2d16682922b79b94709505029cebc8
Brian Bower <brian@brianbower.com>
2020-03-19 18:04:48 -0600

    Add Fragment and Gifs components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Search from './components/layout/Search';
+import Gifs from './components/gifs/Gifs'
 import axios from 'axios';
 import './App.css';
 
@@ -10,14 +11,20 @@ class App extends Component {
   }
   
   searchGifs = async (text) => {
-    const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${text}`)
-    
+    const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${text}`,{ crossDomain: true })
+    this.setState({ gifs: response.data.data })
   }
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Search searchGifs={this.searchGifs} />
+        <div>
+          <Fragment>
+            <Search searchGifs={this.searchGifs} />
+            <Gifs gifs={this.state.gifs} />
+          </Fragment>
+        </div>
       </div>
     )
   }
