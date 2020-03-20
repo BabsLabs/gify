bb2d1131011645f561aba0044e8e7a2d970be47c
Brian Bower <brian@brianbower.com>
2020-03-20 10:34:21 -0600

    Remove styles (moved to index.css)

--- src/components/gifs/GifItem.js
+++ src/components/gifs/GifItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const GifItem = ({ gif: { title, images }}) => {
   return (
-      <img src={images.original.url} alt={title} style={{ width: "200px"}} className="gif" />
+      <img src={images.original.url} alt={title} className="gif" />
   )
 }
 
